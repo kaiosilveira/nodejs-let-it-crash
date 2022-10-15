d8a33da1bb6092869b53b2183696f07ba54fcda3
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-15 10:33:58 +0100

    add outgoing response middleware

---
+++ src/presentation/middleware-chain/outgoing-response/index.test.ts
@@ -0,0 +1,45 @@
+import OutgoingResponseMiddleware from '.';
+
+import * as httpVerbs from '../../http/verbs';
+import * as httpCodes from '../../http/status-codes';
+import FakeExpressFactory from '../../../__mocks__/express/factory';
+import FakeLogger from '../../../monitoring/logger/fake';
+
+describe('OutgoingResponseMiddleware', () => {
+  it('should log request status on res.finish', () => {
+    const next = jest.fn();
+    const method = httpVerbs.GET;
+    const url = 'http://localhost:3000';
+    const req = FakeExpressFactory.createRequest({
+      headers: {},
+      body: {},
+      context: { url, method },
+    });
+
+    const res = FakeExpressFactory.createResponse();
+    const logger = new FakeLogger();
+
+    const status = httpCodes.OK;
+    const data = { ok: 1 };
+
+    const spyOnLogInfo = jest.spyOn(logger, 'info');
+
+    new OutgoingResponseMiddleware({ logger }).hook(req, res, next);
+    const receivedResponse = res.status(status).json(data);
+    expect(receivedResponse.body).toEqual(data);
+    expect(receivedResponse.status).toEqual(status);
+
+    res.emit('finish');
+
+    expect(spyOnLogInfo).toHaveBeenCalledTimes(1);
+    expect(spyOnLogInfo).toHaveBeenCalledWith(
+      expect.objectContaining({
+        message: expect.stringContaining('GET '),
+        durationMs: expect.any(Number),
+        status,
+        method,
+        url,
+      })
+    );
+  });
+});
---
+++ src/presentation/middleware-chain/outgoing-response/index.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import ILogger from '../../../monitoring/logger';
+
+export default class OutgoingResponseMiddleware {
+  _logger: ILogger;
+
+  constructor({ logger }: { logger: ILogger }) {
+    this._logger = logger;
+    this.hook = this.hook.bind(this);
+  }
+
+  hook(req: Request, res: Response, next: Function): void {
+    res.on('finish', () => {
+      const ctx = req.context;
+
+      const durationMs = +new Date() - (ctx?.reqStartedAt ?? 0);
+      this._logger.info({
+        message: `${ctx?.method.toUpperCase()} ${ctx?.url} ${res.statusCode} (${durationMs}ms)`,
+        status: res.statusCode,
+        durationMs,
+        ...req.context,
+      });
+    });
+
+    next();
+  }
+}
