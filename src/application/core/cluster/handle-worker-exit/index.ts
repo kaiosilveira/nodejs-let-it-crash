fdd96cf625f3738e935e54810f408ae285add4c1
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-15 13:17:54 +0100

    minor refactoring

--- src/application/core/cluster/handle-worker-exit/index.ts
+++ src/application/core/cluster/handle-worker-exit/index.ts
@@ -1,24 +1,22 @@
 import { Cluster, Worker } from 'cluster';
 import ILogger from '../../../observability/logger';
 
-const handleWorkerExit = ({ cluster, logger }: { cluster: Cluster; logger: ILogger }) => {
+export type HandleWorkerExitFnProps = { cluster: Cluster; logger: ILogger };
+
+const handleWorkerExit = ({ cluster, logger }: HandleWorkerExitFnProps) => {
   return (worker: Worker, code: number, signal?: number) => {
+    const logProps = { pId: worker.process.pid, at: new Date() };
+
     if (code === 0) {
+      logger.info({ msg: `worker ${worker.process.pid} exited with code '0'`, ...logProps });
+    } else {
       logger.info({
-        msg: `worker ${worker.process.pid} exited with code '0'`,
-        pId: worker.process.pid,
-        at: new Date(),
+        msg: `worker ${worker.process.pid} died with code '${code}' and signal '${signal}'. Forking a new one...`,
+        ...logProps,
       });
-      return;
+
+      cluster.fork();
     }
-
-    logger.info({
-      msg: `worker ${worker.process.pid} died with code '${code}' and signal '${signal}'. Forking a new one...`,
-      pId: worker.process.pid,
-      at: new Date(),
-    });
-
-    cluster.fork();
   };
 };
 
