9af629f3f33b32beec2f178c41d1c63fdc4b2ef5
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-15 10:34:16 +0100

    rename express factory file

---
+++ src/presentation/factory/index.ts
@@ -0,0 +1,26 @@
+import Express from 'express';
+import Helmet from 'helmet';
+import Crypto from 'node:crypto';
+import { ApplicationEnv } from '../../config/env';
+import IncomingRequestMiddleware from '../middleware-chain/incoming-request';
+import OutgoingResponseMiddleware from '../middleware-chain/outgoing-response';
+import ILogger from '../../monitoring/logger';
+
+import PresentationResourcesManager from '../resources';
+
+export default class ExpressAppFactory {
+  static createApp({ env, logger }: { logger: ILogger; env: ApplicationEnv }) {
+    const app = Express();
+
+    app.use(Helmet());
+    app.use(Express.json());
+    app.use(Express.urlencoded({ extended: true }));
+    app.use(new IncomingRequestMiddleware({ env, generateUUID: Crypto.randomUUID }).hook);
+    app.use(new OutgoingResponseMiddleware({ logger }).hook);
+    app.use(
+      PresentationResourcesManager.configureRouter({ env, logger, router: Express.Router() })
+    );
+
+    return app;
+  }
+}
